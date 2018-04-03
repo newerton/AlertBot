ac6e619515d3a3e9cea5fc0b6a9a15385a8ca0b4
XIII1313 <steven-hs@hotmail.com>
2018-04-03 10:30:00 +0200

    Update fbtestbot.py

--- fbtestbot.py
+++ fbtestbot.py
@@ -91,8 +91,8 @@ def test():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     try:
-        _thread.start_new_thread(app.run(host='0.0.0.0', port=port ), ())
         _thread.start_new_thread(test(), ())
+        _thread.start_new_thread(app.run(host='0.0.0.0', port=port ), ())
     
     except:
         print ("Error: unable to start thread")
